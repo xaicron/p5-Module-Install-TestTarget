979625da80b659a2b2d30f048d6a27ccd608a484
xaicron <xaicron@gmail.com>
2010-09-07 02:34:29 +0900

    initial commit

---
+++ lib/Module/Install/TestAssembler.pm
@@ -0,0 +1,42 @@
+package Module::Install::TestAssembler;
+
+use strict;
+use warnings;
+use 5.008_001;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=for stopwords
+
+=head1 NAME
+
+Module::Install::TestAssembler -
+
+=head1 SYNOPSIS
+
+  use Module::Install::TestAssembler;
+
+=head1 DESCRIPTION
+
+Module::Install::TestAssembler is
+
+=head1 AUTHOR
+
+xaicron E<lt>xaicron {at} cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2010 - xaicron
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
